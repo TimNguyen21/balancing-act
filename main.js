ed60c1800abb9056b3e9e8bd0e23cddc18625734
Tim Nguyen <tim.nguyen2112@gmail.com>
2019-12-01 04:13:36 -0700

    Update JavaScript where navigation pane link Transaction page to Main page

--- main.js
+++ main.js
@@ -1,9 +1,43 @@
-var welcomeBox = document.querySelector('div.welcome-box');
+// ***Remove Welcome Button***
+
+var welcomeBox = document.querySelector('.welcome-box');
 var button = document.querySelector('.close-icon');
 
-button.addEventListener('click', removeWelcome)
+button.addEventListener('click', removeWelcome);
 
 function removeWelcome(){
 welcomeBox.remove();
 // welcomeBox.style.display = 'none';
 }
+// ***Remove Welcome Button***
+
+// ***Dashboard Icon Navigation***
+var homeColumn1 = document.querySelector('.home-column1');
+var homeColumn2 = document.querySelector('.home-column2');
+var transColumn1 = document.querySelector('.transaction-column1')
+var transColumn2 = document.querySelector('.trans-column2')
+var dashButton = document.querySelector('.dashboard-icon');
+var transButton = document.querySelector('.transactions-icon')
+
+transButton.addEventListener('click', changeToTransactionPage);
+
+function changeToTransactionPage(){
+  homeColumn1.style.visibility = 'hidden';
+  homeColumn2.style.visibility = 'hidden';
+  dashButton.style.border = 'none';
+  transButton.style.borderLeft =  '4px solid #1CA3BA';
+  transColumn1.style.visibility = 'visible';
+  transColumn2.style.visibility = 'visible';
+}
+
+dashButton.addEventListener('click', changeToDashboard);
+
+function changeToDashboard(){
+  transColumn1.style.visibility = 'hidden';
+  transColumn2.style.visibility = 'hidden';
+  dashButton.style.borderLeft = '4px solid #1CA3BA';
+  transButton.style.borderLeft =  'none';
+  homeColumn1.style.visibility = 'visible';
+  homeColumn2.style.visibility = 'visible';
+}
+// ***Dashboard Icon Navigation***
